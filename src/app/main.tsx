8e3b966bcab5fb04c016e05de79fbf0651822ecd
Nikita Savenko <nikita.savenko.dev@gmail.com>
2024-10-30 14:27:57 +0200

    feat: custom scrollbar in the main layout

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,4 +1,6 @@
 import './index.css'
+import 'simplebar-react/dist/simplebar.min.css'
+import SimpleBar from 'simplebar-react'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -7,11 +9,11 @@ import Header from '@/widgets/header'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <main className="relative mx-auto flex w-full flex-col">
-      <Header classNames={{ wrapper: 'top-0 absolute z-50 px-4' }} />
-      <div className="max-w-[100vw]">
+    <main className="relative mx-auto flex w-full flex-col overflow-hidden">
+      <SimpleBar className="max-h-screen" autoHide={true}>
+        <Header classNames={{ wrapper: 'top-0 absolute z-50 px-6' }} />
         <MainPage />
-      </div>
+      </SimpleBar>
     </main>
   </StrictMode>,
 )
